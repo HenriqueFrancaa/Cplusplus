79210e5182465d7db2d128d62845e0cf0b1711f5
HenriqueFrancaa <henrique23.franca@gmail.com>
2023-07-12 21:32:00 -0300

    .

---
+++ Aprendendo/ponteiro/teste.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int numeros[4] = {3,4,5,7};
+    for(int i = 0; i < 4; i++){
+        cout << numeros[i] << endl;
+    }
+    return 0;
+}
