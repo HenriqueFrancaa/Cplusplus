75607d29643084f65609f664d2735a4ef825d0a1
HenriqueFrancaa <henrique23.franca@gmail.com>
2023-09-06 22:41:30 -0300

    Question solved!

---
+++ Beecrowd/Q3468.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string op;
+    cin >> op;
+
+    if(op[0] == 'I' || op[0] == 'i' || op[0] == 'Q' || op[0] == 'q'){
+        cout << "mais" << endl;
+    }
+    else{
+        cout << "mas" << endl;
+    }
+    return 0;
+}
